d1da160f6628b4e0589057ee5e85fb7060572bf2
Dark-Fahim <itsmunna1066@gmail.com>
2025-11-10 15:00:50 +0600

    update api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 require('dotenv').config()
 // Middleware
 app.use(express.json())
@@ -68,6 +68,29 @@ async function run() {
             }
             const cursor = joinedEventsCollection.find(query).sort({date: 1})
             const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/manage-events', async (req, res) => {
+            const email = req.query.email
+            const query = {}
+            if(email){
+                query.creatorEmail = email
+            }
+            const cursor = eventsCollection.find(query).sort({date: 1})
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // Update events related apis
+        app.patch('/manage-events/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const updatedData = req.body
+            const update = {
+                $set: updatedData
+            }
+            const options = {}
+            const result = await eventsCollection.updateOne(query, update, options)
             res.send(result)
         })
 
